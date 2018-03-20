72a36049120a9d02d67c460a57730ed364e7d8a4
Jordy Lopez <jordizeus93@gmail.com>
2018-03-20 21:27:12 +0000

    Version 1.0
    This is the first commit about the web on  Git

---
+++ php/send.php
@@ -0,0 +1,13 @@
+<?php
+
+    $destination = 'support@ftrenovationslimited.com';
+    $name = $_POST['name'];
+    $mail = $_POST['email'];
+    $message = $_POST['message'];
+
+    $content = 'Name: ' . $name . '\nMail: ' . $mail . '\nMessage: ' . $message;
+
+    mail($destination, 'Customer request', $content);
+    header('Location:gracias.html');
+
+?>
